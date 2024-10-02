1b021dc334fff15305dc4800d626d1931b6b38e7
Flaty <78325079+Flaty@users.noreply.github.com>
2024-10-02 11:50:23 +0300

    Update color_icon.py

--- color_icon.py
+++ color_icon.py
@@ -8,16 +8,13 @@ red, green, blue = image_converter.split()
 red_crop_left = red.crop((50, 0, image_converter.width, image_converter.height))
 red_crop_left_right = red.crop((25, 0, image_converter.width - 25, image_converter.height))
 
-blue_crop_left = blue.crop((50, 0, image_converter.width, image_converter.height))
+blue_crop_left = blue.crop((0, 0, image_converter.width - 50, image_converter.height))
 blue_crop_left_right = blue.crop((25, 0, image_converter.width - 25, image_converter.height))
 
 green_crop = green.crop((25,0,image_converter.width - 25, image_converter.height))
 
 red_blend = Image.blend(red_crop_left,red_crop_left_right, 0.5)
 blue_blend = Image.blend(blue_crop_left, blue_crop_left_right, 0.5)
-
-
-red_blend.save('output.jpg')
 
 image_merge = Image.merge('RGB', (red_blend, green_crop, blue_blend))
 image_merge.thumbnail((80,80))
